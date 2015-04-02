185c83c6480a328e16c3df8eb56268987f73b0c0
杨云博 <404081533@qq.com>
2015-04-02 17:02:48 +0800

    Create aip.java

---
+++ yyb/api/aip.java
@@ -0,0 +1,138 @@
+package yyb.api;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.Map;
+
+import net.sf.json.JSONObject;
+
+public class api {
+	
+    /** 
+     * 发送GET请求 
+     *  
+     * @param url 
+     *            目的地址  
+     * @return 远程响应结果 
+     */  
+	public static JSONObject sendGet(String targetURL){
+		JSONObject jsStr = null;
+		try {
+            URL restServiceURL = new URL(targetURL);
+            HttpURLConnection httpConnection = (HttpURLConnection) restServiceURL.openConnection();
+            httpConnection.setRequestMethod("GET");
+            httpConnection.setRequestProperty("Accept", "application/json");
+
+            if (httpConnection.getResponseCode() != 200) {
+                   throw new RuntimeException("HTTP GET Request Failed with Error code : "
+                                 + httpConnection.getResponseCode());
+            }
+
+            BufferedReader responseBuffer = new BufferedReader(new InputStreamReader(
+                   (httpConnection.getInputStream())));
+            
+            String output;
+            System.out.println("Output from Server:  \n");
+
+            while ((output = responseBuffer.readLine()) != null) {
+                   System.out.println(output);
+                   jsStr = JSONObject.fromObject(output);
+            }
+
+            httpConnection.disconnect();
+
+       } catch (MalformedURLException e) {
+
+            e.printStackTrace();
+
+       } catch (IOException e) {
+
+            e.printStackTrace();
+
+       }
+		return jsStr;
+     }
+	
+    /** 
+     * 发送POST请求 
+     *  
+     * @param url 
+     *            目的地址 
+     * @param parameters 
+     *            请求参数，Map类型。 
+     * @return 远程响应结果 
+     */  
+	public static String sendPost(String url, Map<String, String> parameters) {  
+        String result = "";// 返回的结果  
+        BufferedReader in = null;// 读取响应输入流  
+        PrintWriter out = null;  
+        StringBuffer sb = new StringBuffer();// 处理请求参数  
+        String params = "";// 编码之后的参数  
+        try {  
+            // 编码请求参数  
+            if (parameters.size() == 1) {  
+                for (String name : parameters.keySet()) {  
+                    sb.append(name).append("=").append(  
+                            java.net.URLEncoder.encode(parameters.get(name),  
+                                    "UTF-8"));  
+                }  
+                params = sb.toString();  
+            } else {  
+                for (String name : parameters.keySet()) {  
+                    sb.append(name).append("=").append(  
+                            java.net.URLEncoder.encode(parameters.get(name),  
+                                    "UTF-8")).append("&");  
+                }  
+                String temp_params = sb.toString();  
+                params = temp_params.substring(0, temp_params.length() - 1);  
+            }  
+            // 创建URL对象  
+            java.net.URL connURL = new java.net.URL(url);  
+            // 打开URL连接  
+            java.net.HttpURLConnection httpConn = (java.net.HttpURLConnection) connURL  
+                    .openConnection();  
+            // 设置通用属性  
+            httpConn.setRequestProperty("Accept", "*/*");  
+            httpConn.setRequestProperty("Connection", "Keep-Alive");  
+            httpConn.setRequestProperty("User-Agent",  
+                    "Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 6.1)");  
+            // 设置POST方式  
+            httpConn.setDoInput(true);  
+            httpConn.setDoOutput(true);  
+            // 获取HttpURLConnection对象对应的输出流  
+            out = new PrintWriter(httpConn.getOutputStream());  
+            // 发送请求参数  
+            out.write(params);  
+            // flush输出流的缓冲  
+            out.flush();  
+            // 定义BufferedReader输入流来读取URL的响应，设置编码方式  
+            in = new BufferedReader(new InputStreamReader(httpConn  
+                    .getInputStream(), "UTF-8"));  
+            String line;  
+            // 读取返回的内容  
+            while ((line = in.readLine()) != null) {  
+                result += line;  
+            }  
+        } catch (Exception e) {  
+            e.printStackTrace();  
+        } finally {  
+            try {  
+                if (out != null) {  
+                    out.close();  
+                }  
+                if (in != null) {  
+                    in.close();  
+                }  
+            } catch (IOException ex) {  
+                ex.printStackTrace();  
+            }  
+        }  
+        return result;  
+    }  
+	
+}
